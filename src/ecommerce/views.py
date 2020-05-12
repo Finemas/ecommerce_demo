7b460265a8cc9c3253891e59e8ffba194bd348a1
Jan Provazník <proovaznik@gmail.com>
2020-05-12 21:20:39 +0200

    Add templates

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def home_page(request):
+    return render(request, "home_page.html", {})
+
+def home_page_old(request):
     html_ = """
     <!doctype html>
     <html lang="en">
